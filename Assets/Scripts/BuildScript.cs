721a2e85035821efc18d399f7081218c76010ad3
Erin <121096547+ErinBatemanMaze@users.noreply.github.com>
2023-04-03 16:24:51 +0100

    Stuff to build

---
+++ Assets/Scripts/BuildScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class BuildScript {
+    public static void PerformScriptOnlyBuild() {
+        // Perform any necessary build operations using only scripts
+        // This could include setting build flags, loading scenes, etc.
+        
+        // Perform the actual build
+        BuildPipeline.BuildPlayer(new string[] { }, "Builds/ScriptOnlyBuild", BuildTarget.StandaloneWindows64, BuildOptions.BuildScriptsOnly);
+    }
+}
